5f8875e0aba03832788b7f6594e7aee1e6f8c0ae
Aris Wibowo <aris.wibowo@elysiannxt.com>
2025-09-19 02:48:26 +0700

    Add 1 file

---
+++ docker-images-apps2.py
@@ -0,0 +1,55 @@
+import os
+import time
+
+images = [
+    'elysiannxt/ifrs9-measurement-calculation-loader:11.0.0',
+    'elysiannxt/ifrs9-impairment-calculation-loader:11.0.0',
+    'elysiannxt/historical-pd-calculation-loader:11.0.0',
+    'elysiannxt/historical-lgd-calculation-loader:11.0.0',
+    'elysiannxt/recovery-analysis-calculation-loader:11.0.0',
+    'elysiannxt/titanium-transformer:11.0.1',
+    'elysiannxt/stage-assessor:11.0.0',
+    'elysiannxt/cashflow-generator:11.0.0',
+    'elysiannxt/level-assessor:11.0.0'
+]
+
+choice = input ("Please input the choice (pull/load/save)? ")
+
+if choice.lower() in ("pull", "save", "load"):
+    print(f"-- {choice.capitalize()} Docker Images --")
+    print('Starting...')
+
+    i = 0
+    number = 1
+    while i < len(images):
+        # Set the images name
+        if("empyreanapac" in images[i]):
+            image_name = images[i].replace('empyreanapac/', '')
+        elif("elysiannxt" in images[i]):
+            image_name = images[i].replace('elysiannxt/', '')
+        elif("portainer" in images[i]):
+            image_name = images[i].replace('portainer/', '')
+        elif("wurstmeister" in images[i]):
+            image_name = images[i].replace('wurstmeister/', '')
+        elif("mcr.microsoft.com" in images[i]):
+            image_name = images[i].replace('mcr.microsoft.com/dotnet/', '')
+
+        image_name = image_name.replace(':', '_')
+
+        # Logging to console
+        print(f'{number} - Processing {images[i]} ⏳')
+
+        # Running the command
+        if choice.lower() == "save":
+            run = os.system(f'docker image save -o  {image_name}.tar {images[i]}')
+        elif choice.lower() == "pull":
+            run = os.system(f'docker pull {images[i]}')
+        elif choice.lower() == "load":
+            run = os.system(f'docker load -i {image_name}.tar')
+
+        i += 1
+        number += 1
+
+    print('✅ Finished.')
+else:
+    print('⚠️ Invalid choice, please enter pull, save, or load')
